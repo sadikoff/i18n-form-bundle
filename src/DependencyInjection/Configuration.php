0baaef572496cc56e5fda1ffe48f7c485a892fea
Sadikov Vladimir <sadikoff@gmail.com>
2017-12-26 19:39:18 +0200

    config tree builder optimizations

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -22,19 +22,23 @@ class Configuration implements ConfigurationInterface
         $treeBuilder = new TreeBuilder();
         $root = $treeBuilder->root('koff_i18n_form')->children();
 
+        $convertStringToArray = function ($v) {
+            return preg_split('/\s*[,|]\s*/', $v);
+        };
+
         $locales = $root->arrayNode('locales');
         $locales->defaultValue(['en']);
-        $locales->beforeNormalization()->ifString()->then(function ($v) {return preg_split('/\s*[,|]\s*/', $v); });
+        $locales->beforeNormalization()->ifString()->then($convertStringToArray);
         $locales->requiresAtLeastOneElement();
         $locales->prototype('scalar');
 
         $required = $root->arrayNode('required_locales');
-        $required->beforeNormalization()->ifString()->then(function ($v) {return preg_split('/\s*[,|]\s*/', $v); });
+        $required->beforeNormalization()->ifString()->then($convertStringToArray);
         $required->prototype('scalar');
 
         $excluded = $root->arrayNode('excluded_fields');
         $excluded->defaultValue(['id', 'locale', 'translatable']);
-        $excluded->beforeNormalization()->ifString()->then(function ($v) {return preg_split('/\s*[,|]\s*/', $v); });
+        $excluded->beforeNormalization()->ifString()->then($convertStringToArray);
         $excluded->prototype('scalar');
 
         return $treeBuilder;
