48dfa538c54ea01b3252662174628a3bef60cd96
Vladimir Sadicov <sadikoff@gmail.com>
2019-01-01 09:41:50 +0200

    test

--- src/I18nFormBundle.php
+++ src/I18nFormBundle.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 /**
- * Class I18nFormBundle.
+ * @author Vladimir Sadicov <sadikoff@gmail.com>
  */
 class I18nFormBundle extends Bundle
 {
